01a5e41eab5bf242269513ebb7c055c7a304717f
Francisco Cota <phatwhalemedia@pop-os.localdomain>
2020-08-26 12:56:32 -0700

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,18 @@
+function toggleElement1() {
+    var x = document.getElementById("codeOfEthics");
+    if (x.style.display && x.style.display !== "none") {
+        x.style.display = "none";
+    }else   {
+        x.style.display = "block";
+    }
+}
+
+function tottleElement2() {
+    var x = document.getElementById("conductReview");
+    if (x.style.display && x.style.display !== "none")  {
+        x.style.display = "none";
+    }else   {
+        x.style.display = "block";
+    }
+
+}
